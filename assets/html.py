e66e891a5193561d1ccf01a616ad1c26ff417256
George Perdrizet <george@perdrizet.org>
2025-06-04 09:44:25 -0400

    Refactored interface HTML elements and tool functions.

---
+++ assets/html.py
@@ -0,0 +1,11 @@
+'''HTML elements for Gradio interface.'''
+
+TITLE = (
+    '''
+        <center> 
+            <h1>Letter counter</h1>
+        </center>
+    '''
+)
+
+DESCRIPTION = 'Enter text and a letter to count how many times the letter appears in the text.'
