e97f93285bf1df99c83a02d25dffeb92e65b4c3d
George Perdrizet <george@perdrizet.org>
2025-06-04 23:32:10 -0400

    Added page content extraction and HTML cleaning functions.

--- functions/tools.py
+++ functions/tools.py
@@ -1,12 +1,8 @@
 '''Tool functions for MCP server'''
 
+import json
 import logging
-from urllib.parse import urlparse
 import functions.helper_functions as helper_funcs
-
-FEED_URIS = {}
-RSS_EXTENSIONS = ['xml', 'rss', 'atom']
-COMMON_EXTENSIONS = ['com', 'net', 'org', 'edu', 'gov', 'co', 'us']
 
 
 def get_content(website: str) -> list:
@@ -23,36 +19,13 @@ def get_content(website: str) -> list:
     logger = logging.getLogger(__name__ + '.get_content')
     logger.info('Getting feed content for: %s', website)
 
-    # Find the feed URI
-    feed_uri = None
+    feed_uri = helper_funcs.find_feed_uri(website)
+    logger.info('find_feed_uri() returned %s', feed_uri)
 
-    # If the website contains xml, rss or atom, assume it's an RSS URI
-    if any(extension in website.lower() for extension in RSS_EXTENSIONS):
-        feed_uri = website
-        logger.info('%s looks like a feed URI already - using it directly', website)
-
-    # Next, check the cache to see if we alreay have this feed's URI
-    elif website in FEED_URIS.keys():
-        feed_uri = FEED_URIS[website]
-        logger.info('%s feed URI in cache: %s', website, feed_uri)
-
-    # If neither of those get it - try feedparse if it looks like a url
-    # or else just google it
-    else:
-        if website.split('.')[-1] in COMMON_EXTENSIONS:
-            website_url = website
-            logger.info('%s looks like a website URL', website)
-
-        else:
-            website_url = helper_funcs.get_url(website)
-            logger.info('Google result for %s: %s', website, website_url)
-
-        feed_uri = helper_funcs.get_feed(website_url)
-        logger.info('get_feed() returned %s', feed_uri)
-
-        FEED_URIS[website] = feed_uri
+    if 'No feed found' in feed_uri:
+        return 'No feed found'
 
     content = helper_funcs.parse_feed(feed_uri)
-    logger.info('parse_feed() returned %s', content)
+    logger.info('parse_feed() returned %s entries', len(list(content.keys())))
 
-    return '\n'.join(content)
+    return json.dumps(content)
